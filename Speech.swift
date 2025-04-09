d00ea143f14e6bad7ad542739fd2921f88da6cac
Indica-cool <89488434+Indica-cool@users.noreply.github.com>
2025-04-09 13:48:18 +0200

    Delete Speech.swift

--- Speech.swift
+++
@@ -1,7 +0,0 @@
-import AVFoundation
-
-func speak(text: String) {
-    let utterance = AVSpeechUtterance(string: text)
-    utterance.voice = AVSpeechSynthesisVoice(language: "de-DE")
-    AVSpeechSynthesizer().speak(utterance)
-}
