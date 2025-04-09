6d10f84ddfaf03584fe8fccc4423b53e1747d7e4
Indica-cool <89488434+Indica-cool@users.noreply.github.com>
2025-04-09 12:55:51 +0200

    Add files via upload

---
+++ BatzenOS/ContentView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Willkommen bei BatzenOS!")
+                .font(.title)
+                .padding()
+        }
+    }
+}
